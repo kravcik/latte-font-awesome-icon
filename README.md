63f5caa68b89d265d822a1fab1997c51189df8ec
Pavel Kravčík <kravcik@modulservis.cz>
2022-06-17 10:01:12 +0200

    initial commit

--- README.md
+++ README.md
@@ -1,2 +1,50 @@
 # LatteFontAwesomeIcon
-Simple generator FA icons into Latte templates
+New font awesome icons - latte generator. Replace older https://github.com/kravcik/nette-macro-fontawesome.
+
+# Register Extension
+
+```neon
+latte:
+	extensions: 
+		- Kravcik\LatteFontAwesomeIcon\Extension
+```
+
+You can also set default values, it is quite simple:
+
+```neon
+latte:
+	extensions: 
+		- Kravcik\LatteFontAwesomeIcon\Extension(defaultStyle: far, defaultFixedWidth: false, defaultElement: i)
+```
+
+## Extension parameters
+
+1. `defaultStyle` - choose FA style (_fas|far|fal|fab etc._), default is `fal`
+2. `defaultFixedWidth` - auto adding `fa-fw` to icons, defaults is `true`
+3. `defaultElement` - HTML element to generate icon, default is `span`
+
+## Macro parameters
+Parameters can be named or ordered by numeral indexes (see examples)
+
+1. `color` - color for current icon, generate `text-primary` for bootstrap colors, for others go `color-xxx`
+2. `size` - size for current icon, for numeric generate `fa-2x`, for strings `fa-lg`
+3. `fw` - fixed width `fa-fw` for current icon
+4. `style` - style for current icon (_fas|far|fal|fab etc._)
+5. `class` - icon name without `fa-`
+
+## Examples
+
+Examples depends on default values, so we using default setting (fal, fw, span).
+
+`{icon star}` -> `<span class="fal fa-star fa-fw"></i>`
+`{icon star, primary}` -> `<span class="fal fa-star text-primary fa-fw"></span>`
+`{icon star, red}` -> `<span class="fal fa-star color-red fa-fw"></span>`
+`{icon star, null, lg}` -> `<span class="fal fa-star fa-lg fa-fw"></span>`
+`{icon star, yellow, 2}` -> `<span class="fal fa-star color-yellow fa-2x fa-fw"></span>`
+`{icon star, blue, size: 2, style: far}` -> `<span class="fal fa-star color-blue fa-2x"></span>`
+`{icon star, class: foo, color: green}` -> `<span class="fal fa-star color-green foo"></span>`
+
+
+Package |   PHP   |  Latte  | Font Awesome | Bootstrap |
+:------:|:-------:|:-------:|:------------:|:---------:|
+   v1   | \>=8.1  | \>=3    |      \>5     |     5     |
---
+++ src/Extension.php
@@ -0,0 +1,98 @@
+<?php
+declare(strict_types=1);
+
+namespace Kravcik\LatteFontAwesomeIcon;
+
+use Latte;
+use Nette\Utils\Html;
+
+class Extension extends Latte\Extension
+{
+	public static string $defaultStyle;
+
+	public static bool $defaultFixedWidth;
+
+	public static string $defaultElement;
+
+	private static array $colorClass = [
+		'primary',
+		'secondary',
+		'success',
+		'danger',
+		'warning',
+		'info',
+		'light',
+		'dark',
+		'body',
+		'muted',
+		'white',
+		'black-50',
+		'white-50'
+	];
+
+	public function __construct
+	(
+		?string $defaultStyle,
+		?bool $defaultFixedWidth,
+		?string $defaultElement
+	)
+	{
+		self::$defaultStyle = $defaultStyle ?: 'fal';
+		self::$defaultFixedWidth = $defaultFixedWidth === false ? $defaultFixedWidth : true;
+		self::$defaultElement = $defaultElement ?: 'span';
+	}
+
+
+	public function getTags(): array
+	{
+		return [
+			'icon' => [$this, 'createNode']
+		];
+	}
+
+
+	public function createNode(Latte\Compiler\Tag $tag): Latte\Compiler\Node
+	{		
+		$subject = $tag->parser->parseUnquotedStringOrExpression();
+		$tag->parser->stream->tryConsume(',');
+		$args = $tag->parser->parseArguments();
+
+		return new Latte\Compiler\Nodes\AuxiliaryNode(
+			fn(Latte\Compiler\PrintContext $context) =>
+				$context->format('echo %escape(\Kravcik\LatteFontAwesomeIcon\Extension::render(%node, ...%node));', $subject, $args)
+		);
+	}
+
+
+	public static function render($name, ?string $color = null, ?int $size = null, ?bool $fw = null, ?string $style = null, ?string $class = null): Html
+	{		
+		$param = [];				
+		$param[] = $style ?: self::$defaultStyle;
+		$param[] = 'fa-' . $name;
+		
+		if($fw === true || ($fw === null && self::$defaultFixedWidth === true))
+		{
+			$param[] = 'fa-fw';
+		}
+
+		if($size !== null)
+		{
+			$param[] = is_numeric($size) ? 'fa-' . $size . 'x' : 'fa-' . $size;
+		}
+
+		if(!empty($class))
+		{
+			$param[] = $class;
+		}
+
+		if($color)
+		{
+			$param[] = in_array($color, self::$colorClass, true) ? 'text-' . $color : 'color-' . $color;
+		}
+
+		return Html::el(self::$defaultElement)->class(implode(' ', $param));
+	}
+
+
+	
+}
